d720f08693ac775fd1e21369e5ad17570a8cac08
narse03 <david.besmier@gmail.com>
2026-05-16 17:00:27 +0200

    fix: inline supabase credentials

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,5 +1,7 @@
 import { createClient as createSupabaseClient } from '@supabase/supabase-js'
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '../config'
+
+const SUPABASE_URL = 'https://uiaoqkkefjwgvstuuube.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpYW9xa2tlZmp3Z3ZzdHV1dWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NTM1MjcsImV4cCI6MjA5NDMyOTUyN30.Mxt40c4ZVMb_Flo3j3T5i7rqPLvfOC9B_1RomQ-ckus'
 
 export function createClient() {
   return createSupabaseClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
